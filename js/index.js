b46820bda774e3a5945bcdd3ec0189693a775c20
Olha <romanivna.199627@gmail.com>
2017-07-28 17:44:36 +0000

    fibonacci4

--- js/index.js
+++ js/index.js
@@ -1,36 +1,39 @@
 // By using while
 
-// let x1 = 1;
-// let x2 = 1;
+let x1 = 1;
+let x2 = 1;
 
-// let n = parseInt(prompt('Enter the number'));
+let n = parseInt(prompt('Enter the number(loop):'));
 
-// let i = x1+x2;
-// while (i < n){
-//     var suma = x1+x2;
-//     x1=x2;
-//     x2=suma;
-//     i++;
-// }
-// document.write(suma);
+let i = x1+x2;
+while (i < n){
+    var suma = x1+x2;
+    x1=x2;
+    x2=suma;
+    i++;
+}
+document.write(suma);
+document.write('<br>');
+
 
 
 //Recursion
-// let n = parseInt(prompt('Enter the number:'));
-// function fibonacci(n) {
-//     if(n > 1){
-//         n = fibonacci(n - 1) + fibonacci(n - 2);
-//     }else {
-//         n = n;
-//     }
-//     return n;
-// }
-// document.write(fibonacci(n));
+let m = parseInt(prompt('Enter the number(recursion):'));
+function fibonacci(m) {
+    if(m > 1){
+        m = fibonacci(m - 1) + fibonacci(m - 2);
+    }else {
+        m = m;
+    }
+    return m;
+}
+document.write(fibonacci(m));
+document.write('<br>');
 
 
 
 //Array
-let number = parseInt(prompt('Enter the number:'));
+let number = parseInt(prompt('Enter the number(array):'));
 function fibonacci(n) {
     let numb = [1, 1];
 
