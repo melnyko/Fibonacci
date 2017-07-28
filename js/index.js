63fe3c6afca0b9cf41f8538bccf8b8a3c96ed6d1
Olha <romanivna.199627@gmail.com>
2017-07-28 17:02:04 +0000

    fibonacci

---
+++ js/index.js
@@ -0,0 +1,44 @@
+// By using while
+
+// let x1 = 1;
+// let x2 = 1;
+
+// let n = parseInt(prompt('Enter the number'));
+
+// let i = x1+x2;
+// while (i < n){
+//     var suma = x1+x2;
+//     x1=x2;
+//     x2=suma;
+//     i++;
+// }
+// document.write(suma);
+
+
+//Recursion
+let n = parseInt(prompt('Enter the number:'));
+function fibonacci(n) {
+    if(n > 1){
+        n = fibonacci(n - 1) + fibonacci(n - 2);
+    }else {
+        n = n;
+    }
+    return n;
+}
+document.write(fibonacci(n));
+
+
+
+//Massif
+// let number = parseInt(prompt('Enter the number:'));
+// function fibonacci(n) {
+//     let numb = [1, 1];
+
+//     for (var i = 2; i < n; i++) {
+//         numb[i] = numb[i-1]+ numb[i-2];
+//     }
+//     return numb[n-1];
+// }
+// document.write(fibonacci(number));
+
+
